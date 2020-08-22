cb8692c9064d5ce5a95cf2732e30f6cd9c752267
Sanjeev kumar <sanjeevkr4983@gmail.com>
2020-08-22 16:47:40 +0530

    New information in weather stack added for humidity.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,8 @@ const forecast = (latitude, longitude,  callback) =>{
             callback('Unable to find location!', undefined)
         }
         else{
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currenlty " + body.current.temperature + " degree  , it feels like " + body.current.feelslike+" degree")
+            
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currenlty " + body.current.temperature + " degree  , it feels like " + body.current.feelslike+" degree out. The humidity is " + body.current.humidity + "%.")
 
         }
     })
